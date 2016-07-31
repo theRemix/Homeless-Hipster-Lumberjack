0c8a0bcfd6dae2bcc70c9126da45ea406005b8ba
Jon Borgonia <remix@Titan.local>
2016-07-31 15:52:55 -0700

    add angularfire display some lumberjacks

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app
+  defaultFirebase({
+    apiKey: "",
+    authDomain: "homeless-hipster-lumberjack.firebaseapp.com",
+    databaseURL: "https://homeless-hipster-lumberjack.firebaseio.com",
+    storageBucket: "homeless-hipster-lumberjack.appspot.com",
+  })
+]);
--- src/system-config.ts
+++ src/system-config.ts
@@ -9,10 +9,16 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  'firebase': 'vendor/firebase/firebase.js',
+  'angularfire2': 'vendor/angularfire2'
 };
 
 /** User packages configuration. */
 const packages: any = {
+  angularfire2: {
+    defaultExtension: 'js',
+    main: 'angularfire2.js'
+  }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,6 +42,7 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/display',
   /** @cli-barrel */
 ];
 
